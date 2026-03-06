a9ad5181795ba8ce86e747d0bdad664e1b63feb4
Ankit Kumar Singh <122798317+ankit3890@users.noreply.github.com>
2026-03-06 16:20:13 +0530

    Fix indentation for app.run in main block

--- app.py
+++ app.py
@@ -137,4 +137,4 @@ def leaderboard():
 if __name__ == "__main__":
     # port = int(os.environ.get("PORT", 10000))
     # app.run(host="0.0.0.0", port=port)
-      app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
